43039d3c72582877fb90158ea942ac80f76acd2d
Manon Boiteau <manonboiteau@orange.fr>
2022-02-02 22:30:05 +0100

    footer

--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -1,8 +1,31 @@
 const Footer = () => {
   return (
-    <div>
-      <p>Footer</p>
-    </div>
+    <footer>
+      <div className="wrapper">
+        <div>
+          <p>+33 6 69 03 49 01</p>
+        </div>
+        <div className="footer_social">
+          <p>Follow me on:</p>
+          <div>
+            <a
+              href="https://github.com/manon-boiteau"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+            <a
+              href="https://www.linkedin.com/in/manon-boiteau/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              linkedin
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
   );
 };
 
