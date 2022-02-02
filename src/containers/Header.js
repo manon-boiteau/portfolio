149736bc5df76f901ef5870009553f3e04a7e012
Manon Boiteau <manonboiteau@orange.fr>
2022-02-02 21:39:44 +0100

    header

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,8 +1,23 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
-    <div>
-      <p>Header</p>
-    </div>
+    <header>
+      <div className="wrapper">
+        <div>
+          <Link to="/">
+            <h1>MANON</h1>
+          </Link>
+          <p>Boiteau</p>
+        </div>
+        <nav>
+          <Link to="/projects">Projects</Link>
+          <Link to="/about">About</Link>
+          <Link to="/articles">Articles</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
+      </div>
+    </header>
   );
 };
 
